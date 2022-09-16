d7f40dc4dc0947151df82eca8e5811f61323ef36
Enjoy3325 <enjoy3325@gmail.com>
2022-09-16 18:51:38 +0300

    Creating a separate element
    I create a separate element.
    I went through an array for each ingredient.
    Added an item class element for li via createElement.
    Inserted all <li> with .append(...li)

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,15 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const ulListIngredients = document.querySelector('#ingredients');
+const li = ingredients.map(el => {
+  const itemIngredient = document.createElement('li');
+  itemIngredient.classList.add('.item')
+  itemIngredient.textContent = el;
+  return itemIngredient;
+})
+
+ulListIngredients.append(...li)
+console.log(ingredients);
+
+
