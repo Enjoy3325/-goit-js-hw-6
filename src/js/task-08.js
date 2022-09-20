3a982eeedf0cf28d8be1ff2dd4cc597901b9197d
Enjoy3325 <enjoy3325@gmail.com>
2022-09-20 17:51:56 +0300

    add-hw-06-08-js
    ++

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,17 @@
-
+const form = document.querySelector('.login-form');
+form.addEventListener('submit', onSubmitForm);
+function onSubmitForm(e) {
+    e.preventDefault();
+    const data = {
+        email: e.target.elements.email.value,
+        password: e.target.elements.password.value,
+    };
+    console.log(e.target.elements.email.value);
+    
+    if (data.email === '' || data.password === '') {
+    alert("Please fill in all the fields!")
+    } else {
+        form.reset();
+    }
+    console.log(form);
+}
--- src/js/task-10.js
+++ src/js/task-10.js
@@ -1,3 +1,39 @@
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+// }
+
+
+
+let render = document.querySelector('[data-action="render"]');
+let destroy = document.querySelector('[data-action="destroy"]');
+let  boxes = document.getElementById("boxes");
+render.addEventListener("click", getAmount);
+destroy.addEventListener("click", destroyBoxes);
+
+function getAmount() {
+  let amount = document.querySelector("#controls input").value;
+  createBoxes(amount);
+}
+
+function createBoxes(amount) {
+  let basicSize = 30;
+  let fragment = document.createDocumentFragment();
+  for (let i = 0; i < amount; i+= 1) {
+    let size = basicSize + i * 10;
+    let div = document.createElement("div");
+    div.style.cssText = `width: ${size}px; height: ${size}px; background-color: rgba( ${random()} , ${random()} , ${random()} )`;
+    fragment.appendChild(div);
+  }
+  boxes.appendChild(fragment);
+}
+
+function destroyBoxes() {
+  boxes.innerHTML = "";
+}
+
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
