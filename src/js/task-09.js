5c507e94522852de7e640440188f0a90cc181c11
Enjoy3325 <enjoy3325@gmail.com>
2022-09-21 13:12:08 +0300

    done-06-09-hw-js
    ++

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,33 @@
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const refs = {
+  myBody: document.querySelector('body'),
+  spanColor: document.querySelector('.color'),
+  btnChangeColor: document.querySelector('.change-color'),
+}
+refs.btnChangeColor.addEventListener('click', onChacgeColorClick);
+
+function onChacgeColorClick() {
+
+    let colorChange = getRandomHexColor();
+  refs.myBody.style.backgroundColor = colorChange;
+  refs.spanColor.textContent = colorChange;
+  refs.btnChangeColor.style.background = getRandomHexColor();
+  console.log(refs.myBody);
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
